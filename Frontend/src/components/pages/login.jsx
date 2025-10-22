ba5ce65d14518aaef6eb96c54d2a04ae8a13a505
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-22 20:36:07 +0530

    card content "

--- Frontend/src/components/pages/login.jsx
+++ Frontend/src/components/pages/login.jsx
@@ -237,6 +237,26 @@ export default function Login3D(){
                 <div className="absolute bottom-0 left-0 w-[60px] h-full bg-gradient-to-t from-purple-900/60 to-indigo-900/60 " style={{ transform: 'translateY(30px) rotateX(-90deg)',transformOrigin:'bottom',borderRadius:' 0 0 24px 24px'  }} />
 
 
+                <div className="absolute inset-0 p-10 flex flex-col items-center justify-center pointer-event-none" style={{ transform: 'translateZ(31px)' }} >
+                    <div className="w-full max-w-sm space-y-6 pointer-events-auto " >
+
+                        
+                    <div className="flex justify-center mb-2" >
+                        <div className="w-16 h-16 rounded-full bg-gradient-to-br from-purple-500 to-blue-500 flex-items-center justify-center shadow-lg  shadow-purple-500/30" >
+                                <div className="w-14 h-14 rounded-full bg-gradient-to-br from-purple-950 via-indigo-950 to-violet-950 flex items-center justify-center  ">
+                                    <svg className="w-8 h-8 text-purple-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                                    </svg>
+                                </div>
+                        </div>
+                    </div>
+                        
+
+                    </div>
+
+                </div>
+
+
             </motion.div>
 
             
