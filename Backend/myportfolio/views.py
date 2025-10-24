7f7b1156358537dfdc8201f8f002fdd25999e80d
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-24 13:45:52 +0530

    import in view for rest frameworks

--- Backend/myportfolio/views.py
+++ Backend/myportfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import status
+
 
 # Create your views here.
+
+
