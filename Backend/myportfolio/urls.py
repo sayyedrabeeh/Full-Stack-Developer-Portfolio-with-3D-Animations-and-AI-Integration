2a6e49bc1590d0a3220231e8f14037d9df7c7c86
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-24 19:26:12 +0530

    add json web token in sign signup for authtication in urls

--- Backend/myportfolio/urls.py
+++ Backend/myportfolio/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import signUp, login
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('signup/', signUp, name='signup'),
     path('login/', login, name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
