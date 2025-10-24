d87c736cc677edb7c85e41c8e9e616bca98585e9
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-24 14:05:33 +0530

    sign up drf view added

--- Backend/myportfolio/views.py
+++ Backend/myportfolio/views.py
@@ -6,6 +6,28 @@ from django.contrib.auth import authenticate
 from rest_framework import status
 
 
+
 # Create your views here.
+
+
+@api_view(['POST'])
+def signUp(request):
+    
+    email = request.data.get('email')
+    password = request.data.get('password')
+    confirmPassword = request.data.get('confirmPassword')
+
+    if password != confirmPassword:
+        return Response({'error':'Password not match'},status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(username = email ).exists():
+        return Response({'error':'user already exist '},status=status.HTTP_400_BAD_REQUEST)
+    
+    if len(password) < 6 :
+        return Response({'error':'password must be six character '},status=status.HTTP_400_BAD_REQUEST)
+    
+    user = User.objects.create(username = email , email = email,password =  password)
+
+    return Response({'message':'User created Successfully'},status=status.HTTP_201_CREATED)
 
 
