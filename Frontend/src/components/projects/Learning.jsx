928db8869006bc71acb293a9b7db045c4c7e9fea
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-28 20:25:29 +0530

    added projes jsx for the project

---
+++ Frontend/src/components/projects/Learning.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Learning() {
+    
+    return (
+        <>
+        Learning
+        </>
+    )
+}
---
+++ Frontend/src/components/projects/React.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function React() {
+    
+    return (
+        <>
+        React
+        </>
+    )
+}
