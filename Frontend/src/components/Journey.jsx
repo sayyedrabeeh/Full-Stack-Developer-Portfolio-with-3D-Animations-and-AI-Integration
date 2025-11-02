6ae84625efdffca03210b78a9c5ce0c1fed0e0ae
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-02 16:35:34 +0530

    creating joureny section animation bike with algles etc

--- Frontend/src/components/Journey.jsx
+++ Frontend/src/components/Journey.jsx
@@ -1,6 +1,6 @@
 import React,{ useState,useEffect,useRef } from "react"
-import { motion,AnimatePresence } from "framer-motion"
-import { Bike,X,Sparkles } from "lucide-react"
+import { motion,AnimatePresence, progress } from "framer-motion"
+import { Bike,X,Sparkles, Milestone } from "lucide-react"
 
 
 export default function Journey() {
@@ -25,6 +25,43 @@ export default function Journey() {
         }))
         setParticles(newParticles)
     }, [])
+    useEffect(() => {
+        if (pathRef.current) {
+            setPathLength(pathRef.current.getTotalLength)
+        }
+    }, [])    
+    useEffect(() => {
+
+        if (!pathLength) return
+        const targetProgress = activeMileStone / (mileStones.length - 1)
+        const startProgress = bikeProgress
+        const duration = 2000
+        const startTime = Date.now()
+        
+        const animate = () => {
+            const elapsed = Date.now - startTime
+            const progress = Math.min(elapsed / duration, 1)
+            const eased = progress < 0.5 ? 4 * progress * progress * progress : 1 - Math.pow(-2 * progress + 2, 3) / 2
+            const currentProgress = startProgress + (targetProgress - startProgress) * eased
+            setBikeProgress(currentProgress)
+            
+            if (pathRef.current) {
+                const point = pathRef.current.getPointAtLength(currentProgress * pathLength )
+                const nextPoint = pathRef.current.getPointAtLength(Math.min(currentProgress * pathLength + 10, pathLength))
+                const angle = Math.atan2(nextPoint.y - point.y, nextPoint.x - point.x) * 180 / Math.PI
+                
+                setBikePos({ x: point.x, y: point.y })
+                setBikeAngle(angle)
+            }
+            if (progress < 1) {
+                requestAnimationFrame(animate)
+            }
+        }
+
+
+        animate()        
+
+    }, [activeMileStone, pathLength, mileStones.length])
     
 
     return (
