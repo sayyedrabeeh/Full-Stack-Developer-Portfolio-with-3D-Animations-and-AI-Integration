223013cf9d4c33d94718c66cf07f1f3dfe9cdff4
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-03 17:43:51 +0530

    home button on login card

--- Frontend/src/components/ExternalLinks.jsx
+++ Frontend/src/components/ExternalLinks.jsx
@@ -3,3 +3,10 @@ import { motion } from "framer-motion";
 import { FiGithub, FiLinkedin, FiTwitter, FiMail, FiGlobe, FiCode, FiCopy, FiRss } from 'react-icons/fi'
 import { SiLeetcode } from "react-icons/si";
 
+export default function ExternalLinks() {
+    
+
+    return (<>
+    
+    </>)
+}
