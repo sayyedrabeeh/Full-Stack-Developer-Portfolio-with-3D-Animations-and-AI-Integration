4450cf1e1560c26517e3c30f119651f46a76c5e8
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-06 07:30:09 +0530

    url for hf-proxy

--- Backend/myportfolio/urls.py
+++ Backend/myportfolio/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
  
-from .views import signUp, login,create_project,project_counts,get_projects,toggle_like,add_comment,get_comments,toggle_bookmark,saved_projects,delete_project,add_journey,get_journey,delete_journey 
+from .views import signUp, login,create_project,project_counts,get_projects,toggle_like,add_comment,get_comments,toggle_bookmark,saved_projects,delete_project,add_journey,get_journey,delete_journey,hf_proxy 
  
  
 from rest_framework_simplejwt.views import (
@@ -25,6 +25,7 @@ urlpatterns = [
     path('journey/',get_journey),
     path('journey/add/',add_journey),
     path('journey/delete/<int:id>/',delete_journey),
+    path('api/hf/', hf_proxy, name='hf_proxy'),
 
 
 
