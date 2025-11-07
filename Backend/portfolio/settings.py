65d99ad01eb5c3ad8117f9ddff25857ba3c2a258
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-07 18:40:59 +0530

    subabase connected

--- Backend/portfolio/settings.py
+++ Backend/portfolio/settings.py
@@ -76,11 +76,17 @@ import dj_database_url
 import os
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',        # Supabase DB name
+        'USER': 'postgres',        # Supabase user
+        'PASSWORD': 'Rabeeh@1234',    # Supabase password
+        'HOST': 'db.aptzoxomjcnsihefhkbv.supabase.co', # Supabase host
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',  # Supabase requires SSL
+        },
+    }
 }
 
 
