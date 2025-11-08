e01d2ae1ec3fdfcdd04be80d25d95c883a53ec4e
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-11-08 10:37:02 +0530

    some change add cloudnary

--- Backend/portfolio/settings.py
+++ Backend/portfolio/settings.py
@@ -148,8 +148,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ 
 
 # -----------------------
 # OTHER SETTINGS
