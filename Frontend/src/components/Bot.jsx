3dff3f2ef0a5f8bb49c5c9e56aaca6d360013015
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-12-25 15:26:13 +0530

    gemni api model changed

--- Frontend/src/components/Bot.jsx
+++ Frontend/src/components/Bot.jsx
@@ -17,8 +17,7 @@ export default function ChatBot() {
     const textareaRef = useRef(null);
  
     const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-    const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
-  
+   const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=' + GEMINI_API_KEY;
     const scrollToBottom = () => {
         messagesEndRef.current?.scrollIntoView({behavior:'smooth'})
     }
